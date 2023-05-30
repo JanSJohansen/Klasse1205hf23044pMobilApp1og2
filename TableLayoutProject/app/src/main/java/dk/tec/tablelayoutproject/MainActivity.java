6936deb733491ae84e1a545423c154dd05c934ab
JanSJohansen <116347559+JanSJohansen@users.noreply.github.com>
2023-05-30 13:33:27 +0200

    Commit tirsdag 30.5.23 14

---
+++ TableLayoutProject/app/src/main/java/dk/tec/tablelayoutproject/MainActivity.java
@@ -0,0 +1,14 @@
+package dk.tec.tablelayoutproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
