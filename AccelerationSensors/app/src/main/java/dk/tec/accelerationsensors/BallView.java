fc0921d4ac5f83978fc903613af32db2c15f3a1e
JanSJohansen <116347559+JanSJohansen@users.noreply.github.com>
2023-06-06 10:59:19 +0200

    Commit BallView 6.6.23 kl 11

--- AccelerationSensors/app/src/main/java/dk/tec/accelerationsensors/BallView.java
+++ AccelerationSensors/app/src/main/java/dk/tec/accelerationsensors/BallView.java
@@ -1,20 +1,41 @@
 package dk.tec.accelerationsensors;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.view.View;
 
-public class BallView extends View implements SensorEventListener {
+public class BallView extends View implements SensorEventListener
+{
+    MainActivity main;
     public BallView(MainActivity mainActivity)
     {
         super(mainActivity);
+        main = mainActivity;
+    }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.GREEN);
+        paint.setTextSize(50);
+
+        canvas.drawText("Hej fra Ball", 100, 100, paint);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event)
     {
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        {
+            main.upDateScrenValues(event.values);
+
+            
+        }
 
 
     }
